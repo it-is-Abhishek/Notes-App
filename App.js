ab644ad7b95de85dceb49617e1a079ab4e486af6
it-is-Abhishek <abhishekverma1489@gmail.com>
2025-09-30 02:31:12 +0530

    updated app.js

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+import HomeScreen from './src/screens/HomeScreen';
+import CategoryScreen from './src/screens/CategoryScreen';
+import AddNoteScreen from './src/screens/AddNoteScreen';
+import NoteDetailsScreen from './src/screens/NoteDetailsScreen';
+
+import { NotesProvider } from './src/contexts/NotesContext';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NotesProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Category" component={CategoryScreen} />
+          <Stack.Screen name="AddNote" component={AddNoteScreen} />
+          <Stack.Screen name="NoteDetails" component={NoteDetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NotesProvider>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
