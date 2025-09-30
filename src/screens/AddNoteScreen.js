3b53fcf39734336284d6f86f349243f1f05b1ee6
it-is-Abhishek <abhishekverma1489@gmail.com>
2025-09-30 15:50:56 +0530

    updated AddNoteScreen.js

--- src/screens/AddNoteScreen.js
+++ src/screens/AddNoteScreen.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { NotesContext } from '../contexts/NotesContext';
 
-export default function AddNoteScreen() {
+
+
+export default function AddNoteScreen({ navigation }) {
+
+  const { addNote } = useContext(NotesContext);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleSave = () => {
+    const newNote = {
+      id: Date.now(),
+      title,
+      content,
+    };
+     addNote(newNote);
+    navigation.goBack();
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <TextInput
+        placeholder="Note Title"
+        value={title}
+        onChangeText={setTitle}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Note Content"
+        value={content}
+        onChangeText={setContent}
+        style={[styles.input, { height: 100 }]}
+        multiline
+      />
+      <Button title="Save Note" onPress={handleSave} />
     </View>
   );
 }
@@ -12,7 +44,13 @@ export default function AddNoteScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 16,
+  },
+  input:{
+     borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 6,
   },
 });
